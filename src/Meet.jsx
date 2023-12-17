4d8d83e2ba0380b9aa5ae5ab4d20387558a67625
Salai-Kowshikan <salaikowshikan8743@gmail.com>
2023-12-17 18:33:14 +0530

    Meet page not done

--- src/Meet.jsx
+++ src/Meet.jsx
@@ -5,15 +5,23 @@ export default function MeetPage() {
     return (
         <>
             <Navbar />
-            <div className="flex justify-center flex-col h-[85vh]">
-                <div className="border-accent bg-black border-solid border-[10px] h-full sm:border-[20px] rounded-[10px] flex justify-center items-center w-[50%] flex-3 mt-6">
+            <div className="flex justify-center h-[85vh]">
+                <div className="border-accent bg-black border-solid border-[10px] w-[90%] ml-10 h-full sm:border-[20px] rounded-[10px] flex justify-center items-center flex-3 mt-6">
 
                 </div>
-                <div className="flex-1 h-full">
-                    <img src="public\Volume.png" className="h-[150px] w-[150px]" alt="Volume" />
-                    <img src="public\Volume.png" className="h-[150px] w-[150px]" alt="Volume" />
-                    <img src="public\Volume.png" className="h-[150px] w-[150px]" alt="Volume" />
-                    <img src="public\Volume.png" className="h-[150px] w-[150px]" alt="Volume" />
+                <div className="flex-1 h-full flex-col flex justify-around items-center">
+                    <div className="m-auto mx-2 h-[100px] w-[100px] flex justify-center items-center">
+                        <img src="public\Volume.png" className="h-full w-full" alt="Volume" />
+                    </div>
+                    <div className="m-auto mx-2 h-[100px] w-[100px] flex justify-center items-center">
+                        <img src="public\Camera.png" className="h-full w-full" alt="Volume" />
+                    </div>
+                    <div className="m-auto mx-2 h-[100px] w-[100px] flex justify-center items-center">
+                        <img src="public\Logo.png" className="h-full w-full" alt="Volume" />
+                    </div>
+                    <div className="m-auto mx-2 h-[100px] w-[100px] flex justify-center items-center">
+                        <img src="public\Logout.svg" className="h-full w-full" alt="Volume" />
+                    </div>
                 </div>
             </div>
         </>

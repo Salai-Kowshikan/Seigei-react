b9afb3644d3e9a1b4eb2fda33341939120f521fa
Salai-Kowshikan <salaikowshikan8743@gmail.com>
2024-01-02 19:41:16 +0530

    reduced margin

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -73,7 +73,7 @@ export default function LandingPage() {
         </div>
       </div>
       <div className="w-full min-h-[90svh] my-8 p-8 font-inter text-white text-2xl font-bold">
-        <h1 className="text-[96px] font-extrabold text-center leading-normal">Enter Project Seigei</h1>
+        <h1 className="text-[96px] font-extrabold text-center leading-normal mb-0">Enter Project Seigei</h1>
         <div className="flex">
           <div className="flex-1">
             <p>

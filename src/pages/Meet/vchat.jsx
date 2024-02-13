fb29d72e11b10d12eae5ea87d5e68d1a90b3594b
Salai-Kowshikan <salaikowshikan8743@gmail.com>
2024-02-13 16:57:11 +0530

    Fixed the directory issue with vite

--- src/pages/Meet/vchat.jsx
+++ src/pages/Meet/vchat.jsx
@@ -9,7 +9,7 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog";
+} from '../../Components/ui/dialog';
 
 export default function ChatPage() {
   const [inviteLink, setInviteLink] = useState(null);

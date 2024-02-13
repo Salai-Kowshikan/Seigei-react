b6b6007ba194c304d689a3f04c11922bbac2f7d7
Rishi-kumar18 <rk18102004@gmail.com>
2024-02-13 18:19:15 +0530

    with sessions

--- seigei/index.js
+++ seigei/index.js
@@ -1,17 +1,44 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+require('dotenv').config();
+
+const port = process.env.PORT || 5000;
 const mongoose = require("mongoose");
 const cors = require("cors");
 const Registermodel = require("./models/registerdb");
 const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
-const saltRounds = 10;
+const session = require('express-session');
+const { Console } = require("console");
+//openai
 
+const openai = require('openai');
+
+const saltRounds = 10;
+//json parsers
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json());
-
+//openai
+const openaiConfig = new openai.OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+//sessions
+app.use(session({
+  secret: 'your-secret-key',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { secure: false } // set to true if your using https
+}));
+//session authentication
+function isAuthenticated(req, res, next) {
+  if (req.session.userId) {
+    next();
+  } else {
+    res.json({ message: "Unauthorized" });
+  }
+}
+//dbconnect
 mongoose.connect("mongodb://localhost:27017/seigei", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -54,6 +81,8 @@ app.post("/login", async (req, res) => {
     if (user) {
       const result = await bcrypt.compare(req.body.password, user.password);
       if (result) {
+        console.log("Name:",user.name);
+        req.session.userId = user.name;
         res.json({ message: "Login successful" });
       } else {
         res.json({ message: "Password does not match" });
@@ -65,3 +94,20 @@ app.post("/login", async (req, res) => {
     res.json({ message: err });
   }
 });
+
+app.get('/gpt', async (req, res) => {
+  const words = "my school friend come home food drink";
+  const messages = "Make a meaningful sentence out of the phrases: " + words;
+  console.log(messages);
+  const completion = await openaiConfig.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [
+          {"role": "user", "content": messages}
+      ],
+      max_tokens: 200
+  })
+  console.log(completion);
+  res.send(completion.choices[0].message);
+
+});
+

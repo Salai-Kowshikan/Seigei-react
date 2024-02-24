bdfd747665ac07deec3ac41adca923bbda092e74
Salai-Kowshikan <salaikowshikan8743@gmail.com>
2024-02-24 19:20:02 +0530

    Hope it works

--- src/pages/Interpreter/Upload.jsx
+++ src/pages/Interpreter/Upload.jsx
@@ -1,76 +1,105 @@
-import React, {useRef, useState} from "react";
+import React, { useRef, useState } from "react";
 import Navbar from "../../Components/Navbar/index";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 export default function UploadPage() {
-  const navigate = useNavigate()
-  const fileInput = useRef()
-  const [loader,setLoader] = useState(false)
-  const [thumbnail, setThumbnail] = useState("/upload.svg")
-  let text = ""
+  const navigate = useNavigate();
+  const fileInput = useRef();
+  const [loader, setLoader] = useState(false);
+  const [thumbnail, setThumbnail] = useState("/upload.svg");
+  let text = "";
 
   const url = import.meta.env.VITE_APP_API_URL;
 
   const uploadVideo = async () => {
-    setLoader(true)
-    const file = fileInput.current.files[0]
-    const formData = new FormData()
-    formData.append('video', file)
+    setLoader(true);
+    const file = fileInput.current.files[0];
+    const formData = new FormData();
+    formData.append("video", file);
 
-    try{
-        const response = await axios.post(`${url}/upload`, formData)
-        console.log(response)
-        text = response.data.content
-    } catch(error){
-        console.log("Deiii file la prachana: " + error)
+    try {
+      const response = await axios.post(`${url}/upload`, formData);
+      console.log(response);
+      text = response.data.content;
+    } catch (error) {
+      console.log("Deiii file la prachana: " + error);
     } finally {
-      setLoader(false)
-      navigate(`/download?text=${text}`)
+      setLoader(false);
+      navigate(`/download?text=${text}`);
     }
-
-  }
+  };
 
   const handleFileChange = (event) => {
     const file = event.target.files[0];
     if (file) {
-      console.log("file changed")
+      console.log("file changed");
       setThumbnail(URL.createObjectURL(file));
     }
   };
 
   const handleDownloadAudio = async () => {
-    setLoader(true)
-    const file = fileInput.current.files[0]
-    if (file){
-    const formData = new FormData()
-    formData.append('video', file)
-    try {
-      const response = await axios.post(`${url}/audio_convert`,formData)
-      console.log(response.data)
-    } catch (err){
-      console.log("Audio la prachanaaa")
-    }
+    setLoader(true);
+    const file = fileInput.current.files[0];
+    if (file) {
+      const formData = new FormData();
+      formData.append("video", file);
+      try {
+        const response = await axios.post(`${url}/audio_convert`, formData,{
+          responseType: "blob",
+        });
+        const blob = new Blob([response.data], { type: "video/mp4" });
+        const url = URL.createObjectURL(blob);
+        const link = document.createElement('a');
+
+        link.href = url;
+        link.download = 'audio.mp3';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      } catch (err) {
+        console.log("Audio la prachanaaa");
+      }
     } else {
-      alert("Video upload pandra panda")
+      alert("Video upload pandra panda");
     }
-  }
+  };
   const handleDownloadVideo = async () => {
-    setLoader(true)
-    const file = fileInput.current.files[0]
-    if (file){
-    const formData = new FormData()
-    formData.append('video', file)
-    try {
-      const response = await axios.post(`${url}/download-video`,formData)
-      console.log(response.data)
-    } catch (err){
-      console.log("video la prachanaaa")
-    }
+    setLoader(true);
+    const file = fileInput.current.files[0];
+    if (file) {
+      const formData = new FormData();
+      formData.append("video", file);
+      try {
+        const response = await axios.post(`${url}/download-video`, formData, {
+          responseType: "blob",
+        });
+        // Create a new Blob object from the response data
+        const blob = new Blob([response.data], { type: "video/mp4" });
+
+        // Create an object URL for the Blob
+        const url = URL.createObjectURL(blob);
+        const link = document.createElement('a');
+
+        // Set the href and download attributes of the link
+        link.href = url;
+        link.download = 'video.mp4'; // Or any other filename you want
+  
+        // Append the link to the body
+        document.body.appendChild(link);
+  
+        // Programmatically click the link
+        link.click();
+  
+        // Remove the link from the body
+        document.body.removeChild(link);
+      } catch (err) {
+        console.log("video la prachanaaa");
+      }
     } else {
-      alert("Video upload pandra panda")
+      alert("Video upload pandra panda");
     }
-  }
+  };
 
   return (
     <>
@@ -79,9 +108,7 @@ export default function UploadPage() {
         <div className="md:flex-1 flex-[7] flex justify-center items-center font-inter lg:text-8xl text-6xl w-full font-extrabold text-textc py-6 md:py-16 px-6 flex-shrink-[2]">
           Interpret <br /> with Seigei
         </div>
-        <div
-          className="md:flex-1 flex-[11] flex flex-col md:gap-10 items-center justify-evenly md:justify-around"
-        >
+        <div className="md:flex-1 flex-[11] flex flex-col md:gap-10 items-center justify-evenly md:justify-around">
           <div className="border-accent bg-black border-solid border-[10px] rounded-lg flex justify-center items-center aspect-[16/9] w-[80%] md:mt-12">
             <img
               src={thumbnail}
@@ -90,7 +117,12 @@ export default function UploadPage() {
             />
           </div>
           <div>
-            <input type="file" accept="video/*" ref={fileInput} onChange={handleFileChange} ></input>
+            <input
+              type="file"
+              accept="video/*"
+              ref={fileInput}
+              onChange={handleFileChange}
+            ></input>
             {/* <button
               className="bg-accent font-inter font-bold text-white border-none rounded-lg py-3 md:py-6 text-lg md:text-2xl w-[80%] cursor-pointer"
               onClick={uploadVideo}
@@ -99,14 +131,20 @@ export default function UploadPage() {
               {loader ? "Processing" : "Upload video"}
             </button> */}
             <button className="text-white font-inter font-bold border-none py-4 px-[96px] bg-greenbtn rounded-[10px]">
-            Download Transcript as PDF
-          </button>
-          <button className="text-white font-inter font-bold border-none py-4 px-[96px] bg-greenbtn rounded-[10px]" onClick={handleDownloadAudio}>
-            Download Audio
-          </button>
-          <button className="text-white font-inter font-bold border-none py-4 px-[96px] bg-greenbtn rounded-[10px]" onClick={handleDownloadVideo}>
-            Download video
-          </button>
+              Download Transcript as PDF
+            </button>
+            <button
+              className="text-white font-inter font-bold border-none py-4 px-[96px] bg-greenbtn rounded-[10px]"
+              onClick={handleDownloadAudio}
+            >
+              Download Audio
+            </button>
+            <button
+              className="text-white font-inter font-bold border-none py-4 px-[96px] bg-greenbtn rounded-[10px]"
+              onClick={handleDownloadVideo}
+            >
+              Download video
+            </button>
           </div>
         </div>
       </div>
